0297a877b3e78b2765a8d1fd6d16adbf7ec8f6a5
Chun yu Yao <max32002@users.noreply.github.com>
2016-09-12 21:04:37 +0800

    add new pincode dbo.

--- dbo/__init__.py
+++ dbo/__init__.py
@@ -1,4 +1,3 @@
 from dbo.account import DboAccount
 from dbo.delta import DboDelta
 from dbo.metadata import DboMetadata
-from dbo.schema_version import DboSchemaVersion
---
+++ dbo/pincode.py
@@ -0,0 +1,47 @@
+﻿import logging
+from dbo.basetable import BaseTable
+
+
+#data object for Drive
+#############################################################
+class DboPincode(BaseTable):
+    sql_return_fields = "pincode,token,createdTime"
+    sql_table_name = "drive"
+    sql_primary_key = "pincode"
+    sql_create_table = '''
+CREATE TABLE IF NOT EXISTS `pincode` (
+`pincode`   TEXT NOT NULL PRIMARY KEY,
+`token` TEXT NOT NULL,
+`createdTime` DATETIME NULL
+);
+    '''
+    sql_create_index = ['''
+    ''']
+
+    def __init__(self, db_conn):
+        BaseTable.__init__(self, db_conn)
+
+    def add(self, pincode, token):
+        result = 0
+        out_dic = {}
+        out_dic['error_code'] = ''
+        out_dic['rowcount'] = 0
+        try:
+            # insert master
+            sql = "INSERT INTO pincode (pincode,token,createdTime) VALUES (?,?,datetime('now'));"
+            cursor = self.conn.execute(sql, (pincode,token,))
+
+            self.conn.commit()
+            out_dic['lastrowid'] = cursor.lastrowid
+            result = 1
+        except Exception as error:
+            #except sqlite3.IntegrityError:
+            #except sqlite3.OperationalError, msg:
+            #print("Error: {}".format(error))
+            out_dic['error_code'] = error.args[0]
+            out_dic['error_message'] = "{}".format(error)
+            logging.info("sqlite error: %s", "{}".format(error))
+            logging.info("sql: %s", "{}".format(sql))
+            #raise
+        return result, out_dic
+
