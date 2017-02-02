f1b3612410d632aa89049252c23d0d8d7f69aafe
Chun yu Yao <max32002@users.noreply.github.com>
2017-02-02 09:51:17 +0800

    add new command to reset all

--- app/lib/utils.py
+++ app/lib/utils.py
@@ -6,7 +6,7 @@ def json_dump(_obj, _ascii = False):
     return json.dumps(_obj, ensure_ascii = _ascii)
 
 def get_timestamp():
-    return int(round(time.time()*1000))
+    return int(round(time.time()))
     
 def get_uuid():
     return str(uuid.uuid4())
