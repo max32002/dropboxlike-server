68862ba0f400344c05dfaac2b534200cb855b62d
Chun yu Yao <max32002@users.noreply.github.com>
2017-02-13 05:50:32 +0800

    use accoune security instaed of repo security

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,23 +1,27 @@
 from app.controller.repo import RepoClaimAuthHandler
-from app.controller.repo_security import RepoSecurityUpdateHandler
 from app.controller.auth import AuthHandler
+
+from app.controller.cursor import CursorHandler
+from app.controller.delta import DeltaHandler
+from app.controller.metadata import MetadataHandler
+from app.controller.metadata import ListFolderHandler
+
+from app.controller.filecopymove import FileCopyHandler
+from app.controller.filecopymove import FileMoveHandler
+from app.controller.filedelete import FileDeleteHandler
+from app.controller.filecreatefolder import FileCreateFolderHandler
+
 from app.controller.upload import UploadHandler
 from app.controller.download import DownloadHandler
 from app.controller.files import FilesHandler
 from app.controller.files import ChunkUploadHandler
 from app.controller.files import CommitUploadHandler
+
 from app.controller.account_info import AccountInfoHandler
 from app.controller.account_info import AccountSecurityQuestionHandler
-from app.controller.cursor import CursorHandler
-from app.controller.delta import DeltaHandler
-from app.controller.metadata import MetadataHandler
-from app.controller.metadata import ListFolderHandler
+
 from app.controller.thumbnail import ThumbnailHandler
 from app.controller.favorite import FavoriteHandler
-from app.controller.filecopymove import FileCopyHandler
-from app.controller.filecopymove import FileMoveHandler
-from app.controller.filedelete import FileDeleteHandler
-from app.controller.filecreatefolder import FileCreateFolderHandler
 from app.controller.websocket import WebSocketHandler
 from app.controller.version import VersionHandler
 
--- start.py
+++ start.py
@@ -9,15 +9,23 @@ if __name__ == "__main__":
         (r'/server_info', 'app.controller.VersionHandler'),
         (r'/1/auth/token', 'app.controller.AuthHandler'),
         (r'/1/repo/claim_auth', 'app.controller.RepoClaimAuthHandler'),
-        (r'/1/repo/security_update', 'app.controller.RepoSecurityUpdateHandler'),
+        
         (r'/1/files/list_folder', 'app.controller.ListFolderHandler'),
         (r'/1/files/get_metadata', 'app.controller.MetadataHandler'),
         (r'/1/files/create_folder', 'app.controller.FileCreateFolderHandler'),
         (r'/1/files/delete', 'app.controller.FileDeleteHandler'),
+        # [TODO]:
+        (r'/1/files/permanently_delete', 'app.controller.FileDeleteHandler'),
         (r'/1/files/copy', 'app.controller.FileCopyHandler'),
         (r'/1/files/move', 'app.controller.FileMoveHandler'),
         (r'/1/files/upload', 'app.controller.UploadHandler'),
         (r'/1/files/download', 'app.controller.DownloadHandler'),
+
+        # [TODO]:
+        (r'/1/files/get_thumbnail', 'app.controller.ThumbnailHandler'),
+
+        # [TODO]: for streaming
+        (r'/1/files/get_temporary_link', 'app.controller.MetadataHandler'),
 
         # merge to /1/files/list_folder with new parament.
         #(r'/1/delta', 'app.controller.DeltaHandler'),
@@ -26,12 +34,11 @@ if __name__ == "__main__":
         
         (r'/1/files/chunked_upload(.*)', 'app.controller.ChunkUploadHandler'),
         (r'/1/files/commit_chunked_upload(.*)', 'app.controller.CommitUploadHandler'),
-        (r'/1/files/permanently_delete', 'app.controller.FileDeleteHandler'),
 
         (r'/1/account/info', 'app.controller.AccountInfoHandler'),
         (r'/1/account/security_question', 'app.controller.AccountSecurityQuestionHandler'),
-        (r'/1/thumbnails(.*)', 'app.controller.ThumbnailHandler'),
-        (r'/1/favorite(.*)', 'app.controller.FavoriteHandler'),
+        # [TODO]:
+        (r'/1/favorite', 'app.controller.FavoriteHandler'),
         ]
 
     template_path = os.path.abspath(os.path.join(os.path.dirname(__file__) , 'templates'))
