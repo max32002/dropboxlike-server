9b3cf786e86c1c0bd9a742e729fc65a3e13ecb0d
Chun yu Yao <max32002@users.noreply.github.com>
2017-03-09 06:12:08 +0800

    change version from 1.1 to 1.2

--- app/settings.py
+++ app/settings.py
@@ -16,8 +16,8 @@ def define_app_options():
     define('log_level', default=logging.INFO)
     define('log_backup_path', default=tempfile.gettempdir())
     
-    define('versionCode', 2)
-    define('versionName', "1.1")
+    define('versionCode', 3)
+    define('versionName', "1.2")
 
     default_port = 443
     define('port', default_port)
